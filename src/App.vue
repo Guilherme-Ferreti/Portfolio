8df86c097ce90642bc1e5680edf662b26c2c46bf
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-21 14:44:16 -0300

    style: template element as first element in files

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,3 @@
-<script setup lang="ts">
-import TheNavbar from './components/TheNavbar.vue';
-import TheSidebar from './components/TheSidebar.vue';
-</script>
-
 <template>
   <div
     class="grid gap-y-[var(--spacing-screen-padding)] lg:grid-cols-[[sidebar-start]_auto_[sidebar-end]_1fr_[navbar-start]_auto_[navbar-end]] lg:items-start"
@@ -18,3 +13,8 @@ import TheSidebar from './components/TheSidebar.vue';
     <TheSidebar class="lg:col-start-[sidebar-start] lg:col-end-[sidebar-end] lg:row-start-1" />
   </div>
 </template>
+
+<script setup lang="ts">
+import TheNavbar from './components/TheNavbar.vue';
+import TheSidebar from './components/TheSidebar.vue';
+</script>
