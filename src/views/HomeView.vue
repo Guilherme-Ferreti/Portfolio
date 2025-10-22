5d35bbccd87a7cd17af42b1ed5f749c255d1049b
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-22 09:08:20 -0300

    feat: add home hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="px-[var(--spacing-screen-padding)]">
-    <p class="bg-white">
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id hic consectetur doloribus animi
-      ex, accusamus ad a rerum nostrum at, atque nesciunt autem illo assumenda adipisci aperiam
-      iusto voluptatum saepe!
-    </p>
+    <section
+      class="bg-white p-2 lg:p-4 @container"
+      aria-labelledby="hero__heading"
+    >
+      <div class="@xl:w-[60%]">
+        <h1
+          class="text-dark text-4xl/2.75 font-bold mb-1 flex flex-col"
+          id="hero__heading"
+        >
+          <span>I'm Guilherme Ferreti</span>
+          <span><span class="text-primary">Fullstack</span> Developer</span>
+        </h1>
+        <p>
+          Web developer with over 6 years of experience specializing in PHP, with a strong focus on
+          Laravel and Vue.js for building dynamic, scalable, and high-performance applications.
+        </p>
+      </div>
+    </section>
   </div>
 </template>
