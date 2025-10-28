dd067477b72759b4f1f9d1525e5f9ca2e237fe3b
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-28 09:30:54 -0300

    feat: add footer

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div
-    class="grid gap-y-[var(--spacing-screen-padding)] lg:grid-cols-[[sidebar-start]_auto_[sidebar-end]_1fr_[navbar-start]_auto_[navbar-end]] lg:items-start max-w-[var(--screen-max-width)] mx-auto"
+    class="grid max-w-[var(--screen-max-width)] mx-auto lg:grid-cols-[[sidebar-start]_auto_[sidebar-end]_1fr_[navbar-start]_auto_[navbar-end]] lg:items-start"
   >
     <header
-      class="lg:col-start-[navbar-start] lg:col-end-[navbar-end] lg:row-start-1 lg:sticky lg:top-0"
+      class="mb-[var(--spacing-screen-padding)] lg:col-start-[navbar-start] lg:col-end-[navbar-end] lg:row-start-1 lg:sticky lg:top-0 lg:mb-0"
     >
       <TheNavbar />
     </header>
-    <main id="main-content">
-      <RouterView />
-    </main>
     <div
       class="lg:col-start-[sidebar-start] lg:col-end-[sidebar-end] lg:row-start-1 lg:sticky lg:top-0"
     >
-      <TheSidebar class="" />
+      <TheSidebar />
+    </div>
+    <div>
+      <main
+        id="main-content"
+        class="mb-[var(--spacing-screen-padding)]"
+      >
+        <RouterView />
+      </main>
+      <TheFooter />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import TheFooter from './components/TheFooter.vue';
 import TheNavbar from './components/TheNavbar.vue';
 import TheSidebar from './components/TheSidebar.vue';
 </script>
---
+++ src/components/TheFooter.vue
@@ -0,0 +1,5 @@
+<template>
+  <footer class="px-[var(--spacing-screen-padding)]">
+    <p class="bg-white text-center p-1">Guilherme Ferreti &copy; {{ new Date().getFullYear() }}</p>
+  </footer>
+</template>
