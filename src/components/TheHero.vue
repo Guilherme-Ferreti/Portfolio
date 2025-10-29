d739cf3668176f9dcfb1df5027b6aaf3bed7212c
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-29 08:47:19 -0300

    refactor: move hero section to component

---
+++ src/components/TheHero.vue
@@ -0,0 +1,26 @@
+<template>
+  <AppSection
+    :id="AppSectionId.HERO"
+    class="bg-white p-2 lg:p-4 @container"
+    aria-labelledby="hero__heading"
+  >
+    <div class="@xl:w-[60%]">
+      <h1
+        class="text-dark text-4xl/2.75 font-bold mb-1 flex flex-col"
+        id="hero__heading"
+      >
+        <span>I'm Guilherme Ferreti</span>
+        <span><span class="text-primary">Fullstack</span> Developer</span>
+      </h1>
+      <p>
+        Web developer with over 6 years of experience specializing in PHP, with a strong focus on
+        Laravel and Vue.js for building dynamic, scalable, and high-performance applications.
+      </p>
+    </div>
+  </AppSection>
+</template>
+
+<script setup lang="ts">
+import { AppSectionId } from '@/enums/AppSectionId';
+import AppSection from './AppSection.vue';
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,24 +1,6 @@
 <template>
   <div class="px-[var(--spacing-screen-padding)] space-y-3">
-    <AppSection
-      :id="AppSectionId.HERO"
-      class="bg-white p-2 lg:p-4 @container"
-      aria-labelledby="hero__heading"
-    >
-      <div class="@xl:w-[60%]">
-        <h1
-          class="text-dark text-4xl/2.75 font-bold mb-1 flex flex-col"
-          id="hero__heading"
-        >
-          <span>I'm Guilherme Ferreti</span>
-          <span><span class="text-primary">Fullstack</span> Developer</span>
-        </h1>
-        <p>
-          Web developer with over 6 years of experience specializing in PHP, with a strong focus on
-          Laravel and Vue.js for building dynamic, scalable, and high-performance applications.
-        </p>
-      </div>
-    </AppSection>
+    <TheHero />
     <AppSection
       :id="AppSectionId.SKILLS"
       title="My Skills"
@@ -44,6 +26,7 @@
 import AppSection from '@/components/AppSection.vue';
 import EducationHistory from '@/components/EducationHistory.vue';
 import SkillsCarousel from '@/components/SkillsCarousel.vue';
+import TheHero from '@/components/TheHero.vue';
 import WorkHistory from '@/components/WorkHistory.vue';
 import { AppSectionId } from '@/enums/AppSectionId';
 </script>
