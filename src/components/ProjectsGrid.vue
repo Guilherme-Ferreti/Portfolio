10cff554e9432726dda879723c916d9d2073d31f
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-29 17:43:01 -0300

    feat: add more projects

--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -19,7 +19,7 @@
             class="size-[1.5rem]"
           />
         </div>
-        <div class="grid gap-0.75 mt-auto @sm:grid-cols-2">
+        <div :class="{ 'grid gap-0.75 mt-auto': true, '@sm:grid-cols-2': project.url }">
           <AppLink
             :to="project.githubUrl"
             text="See on Github"
@@ -28,6 +28,7 @@
             color="secondary"
           />
           <AppLink
+            v-if="project.url"
             text="Live View"
             :to="project.url"
             :icon="IconExternalLink"
@@ -49,10 +50,14 @@
 </template>
 
 <script setup lang="ts">
+import CSSIcon from '@/assets/images/icons/css.svg';
 import FilamentPHPIcon from '@/assets/images/icons/filamentphp.svg';
+import HTMLIcon from '@/assets/images/icons/html.svg';
+import JavascriptIcon from '@/assets/images/icons/javascript.svg';
 import LaravelIcon from '@/assets/images/icons/laravel.svg';
 import PHPStanIcon from '@/assets/images/icons/phpstan.svg';
 import SASSIcon from '@/assets/images/icons/sass.svg';
+import VueIcon from '@/assets/images/icons/vue.svg';
 import { IconBrandGithubFilled, IconExternalLink } from '@tabler/icons-vue';
 import AppLink from './AppLink.vue';
 
@@ -64,7 +69,33 @@ const projects = [
     image: new URL('@/assets/images/projects/skillbridge.webp', import.meta.url).href,
     technologies: [LaravelIcon, SASSIcon, FilamentPHPIcon, PHPStanIcon],
     githubUrl: 'https://github.com/Guilherme-Ferreti/Skillbridge',
-    url: 'https://skillbridge-guilherme-ferreti.laravel.cloud/',
+    url: 'https://skillbridge-guilherme-ferreti.laravel.cloud',
+  },
+  {
+    name: 'Aleague API',
+    description:
+      'Create and manage soccer tournaments quickly and easily. Choose from various competition formats, add participating teams, and enter results as you wish.',
+    image: new URL('@/assets/images/projects/aleague-api.webp', import.meta.url).href,
+    technologies: [LaravelIcon, PHPStanIcon],
+    githubUrl: 'https://github.com/Guilherme-Ferreti/Aleague-API',
+    url: null,
+  },
+  {
+    name: 'Recipes App',
+    description: 'A collection of delicious and easy-to-cook recipes to satisfy your cravings.',
+    image: new URL('@/assets/images/projects/recipes-app.webp', import.meta.url).href,
+    technologies: [VueIcon, SASSIcon],
+    githubUrl: 'https://github.com/Guilherme-Ferreti/Recipes-App',
+    url: 'https://guilherme-ferreti.github.io/Recipes-App',
+  },
+  {
+    name: 'Diverse HTML Pages',
+    description:
+      'A collection of HTML pages to practice and improve my skills in HTML, CSS, and JavaScript.',
+    image: new URL('@/assets/images/projects/diverse-html-pages.webp', import.meta.url).href,
+    technologies: [HTMLIcon, CSSIcon, JavascriptIcon],
+    githubUrl: 'https://github.com/Guilherme-Ferreti/html',
+    url: 'https://guilherme-ferreti.github.io/html',
   },
 ];
 </script>
