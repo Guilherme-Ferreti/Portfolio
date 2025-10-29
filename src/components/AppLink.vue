38402cef4ba175f989c86a4a42c0e27a124fbdf4
Guilherme Ferreti <guiferretis@gmail.com>
2025-10-29 15:54:30 -0300

    feat: improve project grid layout

--- src/components/AppLink.vue
+++ src/components/AppLink.vue
@@ -5,6 +5,7 @@
     :href="to.toString()"
     target="_blank"
     rel="external"
+    :data-color="color"
   >
     <span v-if="text">{{ text }}</span>
     <component
@@ -31,11 +32,19 @@
 import { computed } from 'vue';
 import type { RouteLocationRaw } from 'vue-router';
 
-const props = defineProps<{
-  to: RouteLocationRaw;
-  text?: string;
-  icon?: unknown;
-}>();
+const props = withDefaults(
+  defineProps<{
+    to: RouteLocationRaw;
+    text?: string;
+    icon?: unknown;
+    color?: 'primary' | 'secondary';
+  }>(),
+  {
+    text: undefined,
+    icon: undefined,
+    color: 'primary',
+  },
+);
 
 const isExternal = computed(() => typeof props.to === 'string' && props.to.startsWith('http'));
 </script>
@@ -44,6 +53,14 @@ const isExternal = computed(() => typeof props.to === 'string' && props.to.start
 @reference '@/assets/css/app.css';
 
 .app-link {
-  @apply flex flex-wrap items-center justify-center gap-1 bg-primary p-0.5 text-sm font-bold text-dark hover:text-white focus-visible:text-white;
+  @apply flex flex-wrap items-center justify-center gap-1 p-0.5 text-sm font-bold;
+}
+
+.app-link[data-color='primary'] {
+  @apply bg-primary text-dark hover:text-white focus-visible:text-white;
+}
+
+.app-link[data-color='secondary'] {
+  @apply bg-gray-600 text-dark;
 }
 </style>
--- src/components/ProjectsGrid.vue
+++ src/components/ProjectsGrid.vue
@@ -1,28 +1,47 @@
 <template>
-  <ul class="grid gap-1 bg-white p-2 @container">
+  <ul class="grid gap-1.25 bg-white p-2 @container">
     <li
       v-for="(project, index) in projects"
       :class="{
-        'bg-white flex flex-col @2xl:flex-row border border-gray-600 rounded-lg': true,
-        'flex-row-reverse': index % 2 === 1,
+        'bg-white flex flex-col @2xl:[&>*]:flex-1 border border-gray-600 rounded-lg hover:bg-gray-100': true,
+        '@2xl:flex-row': index % 2 === 0,
+        '@2xl:flex-row-reverse': index % 2 === 1,
       }"
     >
-      <div class="overflow-hidden flex-1">
+      <div class="p-1.5 gap-1.25 flex flex-col">
+        <h3 class="text-dark text-xl font-bold text-center">{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+        <div class="flex gap-0.75 items-center">
+          <span class="text-dark text-sm font-bold">Technologies:</span>
+          <component
+            v-for="technology in project.technologies"
+            :is="technology"
+            class="size-[1.5rem]"
+          />
+        </div>
+        <div class="grid gap-0.75 mt-auto @sm:grid-cols-2">
+          <AppLink
+            :to="project.githubUrl"
+            text="See on Github"
+            :icon="IconBrandGithubFilled"
+            :aria-label="`See ${project.name} on Github`"
+            color="secondary"
+          />
+          <AppLink
+            text="Live View"
+            :to="project.url"
+            :icon="IconExternalLink"
+            :aria-label="`See ${project.name} live view`"
+          />
+        </div>
+      </div>
+      <div class="overflow-hidden">
         <img
           :src="project.image"
           :alt="`${project.name} homepage screenshot`"
-          class="w-full h-full object-cover bg-no-repeat hover:scale-105 transition-transform duration-300 ease-in-out"
+          class="h-full object-cover bg-no-repeat hover:scale-105 transition-transform duration-300 ease-in-out"
           role="presentation"
           loading="lazy"
-        />
-      </div>
-      <div class="p-1.5 gap-1 flex flex-col flex-1">
-        <h3 class="text-dark text-xl font-bold text-center">{{ project.name }}</h3>
-        <p>{{ project.description }}</p>
-        <AppLink
-          text="View Project"
-          :to="project.url"
-          class="mt-auto"
         />
       </div>
     </li>
@@ -30,6 +49,11 @@
 </template>
 
 <script setup lang="ts">
+import FilamentPHPIcon from '@/assets/images/icons/filamentphp.svg';
+import LaravelIcon from '@/assets/images/icons/laravel.svg';
+import PHPStanIcon from '@/assets/images/icons/phpstan.svg';
+import SASSIcon from '@/assets/images/icons/sass.svg';
+import { IconBrandGithubFilled, IconExternalLink } from '@tabler/icons-vue';
 import AppLink from './AppLink.vue';
 
 const projects = [
@@ -38,6 +62,8 @@ const projects = [
     description:
       'A fictional platform that helps you unlock your creative potential by offering a wide range of online design and development courses.',
     image: new URL('@/assets/images/projects/skillbridge.webp', import.meta.url).href,
+    technologies: [LaravelIcon, SASSIcon, FilamentPHPIcon, PHPStanIcon],
+    githubUrl: 'https://github.com/Guilherme-Ferreti/Skillbridge',
     url: 'https://skillbridge-guilherme-ferreti.laravel.cloud/',
   },
 ];
